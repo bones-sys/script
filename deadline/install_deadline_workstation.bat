c0935abf84f141f168fbd92061397fcbfeb2a260
nagoyabones <nagoya@bones.co.jp>
2024-12-12 20:09:59 +0900

    Fix: quotation marks

--- deadline/install_deadline_workstation.bat
+++ deadline/install_deadline_workstation.bat
@@ -2,7 +2,7 @@
 
 for /f "tokens=3 delims=\ " %%i in ('whoami /groups^|find "Mandatory"') do set LEVEL=%%i
 if NOT "%LEVEL%"=="High" (
-powershell.exe -NoProfile -ExecutionPolicy RemoteSigned -Command "Start-Process %~f0 -Verb runas"
+powershell.exe -NoProfile -ExecutionPolicy RemoteSigned -Command "Start-Process '%~f0' -Verb runas"
 exit
 )
 
--- machine_setup/setting_ip.bat
+++ machine_setup/setting_ip.bat
@@ -1,9 +1,9 @@
 @echo off
 
-for /f "tokens=3 delims=\\ " %%i in ('whoami /groups^|find "Mandatory"') do set LEVEL=%%i
+for /f "tokens=3 delims=\ " %%i in ('whoami /groups^|find "Mandatory"') do set LEVEL=%%i
 if NOT "%LEVEL%"=="High" (
-    powershell.exe -NoProfile -ExecutionPolicy RemoteSigned -Command "Start-Process %~f0 -Verb runas"
-    exit
+powershell.exe -NoProfile -ExecutionPolicy RemoteSigned -Command "Start-Process '%~f0' -Verb runas"
+exit
 )
 
 REM ƒzƒXƒg–¼‚ðŽæ“¾
