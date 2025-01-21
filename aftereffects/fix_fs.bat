5f6d32ec9b878c480b60bb3309ccc88b8e0b86f8
sintaro-saruwatari <saruwatari@bones.co.jp>
2025-01-21 19:56:01 +0900

    Add: fix_fs.bat

---
+++ aftereffects/fix_fs.bat
@@ -0,0 +1,18 @@
+@echo off
+
+for /f "tokens=3 delims=\ " %%i in ('whoami /groups^|find "Mandatory"') do set LEVEL=%%i
+if NOT "%LEVEL%"=="High" (
+powershell.exe -NoProfile -ExecutionPolicy RemoteSigned -Command "Start-Process %~f0 -Verb runas"
+exit
+)
+
+echo --------------------------------------------
+echo ƒpƒX‚ð‹ºˆÐ‚Ì•ÛŒì‚Ì‘ÎÛ‚©‚çœŠO’†‚Å‚·...
+
+powershell Add-MpPreference -ExclusionPath 'C:\Program Files\Adobe\Common\Plug-ins\7.0\MediaCore\F''s Plugins'
+
+echo --------------------------------------------
+echo.
+echo.
+
+timeout /t 1 /nobreak >nul
