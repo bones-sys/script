dc66e1c3ebf3b684e38f64bf2b3145c727c03ff7
Ryuya-Minami <ryuya.minami@bones.co.jp>
2025-05-08 16:45:58 +0900

    Add: create before_re_install.bat script for user folder cleanup
    - Added a new batch script to check for and delete the Shotgun user folder.
    - The script verifies user permissions and executes with elevated privileges if necessary.
    - It also removes the corresponding registry entry for Shotgun Software.

---
+++ ptr_desktop/before_re_install.bat
@@ -0,0 +1,28 @@
+@echo off
+
+for /f "tokens=3 delims=\ " %%i in ('whoami /groups^|find "Mandatory"') do set LEVEL=%%i
+if NOT "%LEVEL%"=="High" (
+powershell.exe -NoProfile -ExecutionPolicy RemoteSigned -Command "Start-Process %~f0 -Verb runas"
+exit
+)
+
+REM ユーザーフォルダのパスを設定
+set TARGET_DIR=%USERPROFILE%\AppData\Roaming\Shotgun
+
+echo --------------------------------------------
+REM 存在確認と削除処理
+if exist "%TARGET_DIR%" (
+    echo フォルダを削除しています: "%TARGET_DIR%"
+    rmdir /s /q "%TARGET_DIR%"
+    echo 削除が完了しました。
+) else (
+    echo フォルダは存在しません: "%TARGET_DIR%"
+)
+
+REG DELETE "HKCU\Software\Shotgun Software\tk-desktop" /f
+echo Shotgun Software\tk-desktop レジストリを削除しました。
+echo --------------------------------------------
+echo.
+echo.
+
+timeout /t 2 /nobreak >nul
